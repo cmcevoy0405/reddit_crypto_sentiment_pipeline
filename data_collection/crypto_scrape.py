9eecdccd606448c7b7f95f2221c28ae4be7f6ccc
cmcevoy0405 <callum.mcevoy01@gmail.com>
2026-01-06 17:36:53 +0000

    Modified crypto scrape to upload to big query

--- data_collection/crypto_scrape.py
+++ data_collection/crypto_scrape.py
@@ -1,7 +1,24 @@
 import requests
 import pandas as pd
 from datetime import datetime
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # load .env first
+
+# immediately overwrite environment variable
+project_id = os.getenv('PROJECT_ID')
+dataset_id = os.getenv('DATASET_ID2')
+cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred_path
+
+print("Using credentials:", cred_path)
+print("File exists?", os.path.isfile(cred_path))
+
+from google.cloud import bigquery  # import after setting env
+client = bigquery.Client(project=os.getenv("PROJECT_ID"))
 
 # -----------------------------
 # CONFIG
@@ -39,13 +56,25 @@ def fetch_last_hour(symbol):
         }
         
 
+def upload_to_bigquery(df):
+    table_id = f"{project_id}.{dataset_id}.raw_crypto"
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+        autodetect=True
+    )
+
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()  # wait for the job to finish
+    print(f"Uploaded {len(df)} rows to {table_id}")
+
+
 if __name__ == "__main__":
     rows = []
-
     for asset in ASSET:
-        row = fetch_last_hour(asset)
-        rows.append(row)
-    
+        rows.append(fetch_last_hour(asset))
+
     df = pd.DataFrame(rows)
-    print(df)
+    print(df) 
+    upload_to_bigquery(df)
+    
 
