287cb9c2b4e033eafc96370bfc493616eac35b97
cmcevoy0405 <callum.mcevoy01@gmail.com>
2026-01-26 20:51:40 +0000

    Added dim and facts tables for power BI

---
+++ reddit_crypto_dbt/models/marts/dim_crypto.sql
@@ -0,0 +1,17 @@
+WITH dim_crypto AS(
+    SELECT 
+        crypto_id,
+        symbol,
+    FROM {{ref('stg_crypto')}}
+)
+SELECT  
+    crypto_id,
+    symbol,
+    CASE 
+    WHEN symbol = 'BNBUSDT' then 'Binance Coin'
+    WHEN symbol = 'BTCUSDT' then 'Bitcoin'
+    WHEN symbol = 'ETHUSDT' then 'Ethereum'
+    WHEN symbol = 'SOLUSDT'  then 'Solana'
+    WHEN symbol = 'XRPUSDT' then 'XRP'
+    END AS crypto_name
+FROM dim_crypto
