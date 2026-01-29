ed52634a4d8264a8882ae13370c46d3fb2a9cf90
cmcevoy0405 <callum.mcevoy01@gmail.com>
2026-01-29 11:18:41 +0000

    Renamed int_posts_mentions and created a counter for each crypto mention

---
+++ reddit_crypto_dbt/models/intermediate/int_posts_mentions.sql
@@ -0,0 +1,30 @@
+WITH posts AS(
+    SELECT 
+        post_id,
+        post_time,
+        LOWER(CONCAT(
+            COALESCE(post_title, ''),
+            ' ',
+            COALESCE(body_text, '')
+        )) as full_post_text,
+        upvote_ratio
+    FROM {{ ref('stg_reddit__posts') }}
+)
+ SELECT
+    post_id,
+    post_time,
+    upvote_ratio,
+    full_post_text,
+
+  -- Mentions per coin
+  ARRAY_LENGTH(REGEXP_EXTRACT_ALL(full_post_text, r'\b(btc|bitcoin|btcusdt)\b')) AS btc_mention_count,
+  ARRAY_LENGTH(REGEXP_EXTRACT_ALL(full_post_text, r'\b(eth|ethereum|ethusdt)\b')) AS eth_mention_count,
+  ARRAY_LENGTH(REGEXP_EXTRACT_ALL(full_post_text, r'\b(sol|solana|solusdt)\b')) AS sol_mention_count,
+  ARRAY_LENGTH(REGEXP_EXTRACT_ALL(full_post_text, r'\b(xrp|ripple|xrpusdt)\b')) AS xrp_mention_count,
+  ARRAY_LENGTH(REGEXP_EXTRACT_ALL(full_post_text, r'\b(bnb|binance\scoin|bnbusdt)\b')) AS bnb_mention_count
+
+FROM posts
+
+
+
+    
